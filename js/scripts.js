790d75c362e78175b93149da06444bbd27d4fd4c
jasminetaylor2 <51241456+jasminetaylor2@users.noreply.github.com>
2019-07-22 13:10:39 -0400

    use fetch to retieve ajax,

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,87 @@
 
 // 
+$(".header-text-container").prepend(`<h3>JASMINE TAYLOR'S</h3>`);
 
+
+//Search Markup
+$(".search-container").html(`<form action="#" method="get">
+                            <input type="search" id="search-input" class="search-input" placeholder="Search...">
+                            <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
+                        </form>`);
+
+// Make an AJAX request
 let xhr = new XMLHttpRequest();
+const url = "https://randomuser.me/api/?results=13&inc=name,email,location,picture";
 xhr.onreadystatechange = function () {
+
     if (xhr.readyState === 4) {
-        // let employees = JSON.parse(xhr.responseText   
-        //  console.log(xhr.responseText);
-        $.ajax({
-            url: 'https://randomuser.me/api/?results=13&inc=name,email,location,picture',
-            dataType: 'json',
-            success: function (data) {
-                console.log(data);
-            }
-        });
+        let employeeResponse = fetch(url)
+            .then(response => response.json())
+            .then(json => () => { 
+              
+
+            });
+        console.log(employeeResponse);
+        // let e = employeeResponse.map(() => { });
+
+        // $.ajax({
+        //     url: 'https://randomuser.me/api/?results=13&inc=name,email,location,picture',
+        //     dataType: 'json',
+        //     success: function (data) {
+        //         let response = data.results;
+        //         for (let i in response) {
+        //             let randEmployee = response[i];
+        //             let name = randEmployee.name;
+        //             let location = randEmployee.location;
+        //             let contact = data.results.email;
+        //             //return response;
+        //             //console.log(response)
+        //             //  console.log(randEmployee[i]);
+        //             console.log(randEmployee);
+        //         }
+
+        //     }
+        // });
+
     }
+
 }
 xhr.open('GET', 'https://randomuser.me');
 xhr.send();
-//typeof
+
 
 //display users that AJAX GET
 
-{/* <div class="card-img-container">
-    <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-                    </div>
-    <div class="card-info-container">
-        <h3 id="name" class="card-name cap">first last</h3>
-        <p class="card-text">email</p>
-        <p class="card-text cap">city, state</p>
-    </div>
-</div> */}
+//Create gallery Div
+function generateHTML(results) {
+    $(".gallery").html(`<div class="card"></div>`);
+    $(".card").append(`<div class="card-img-container"></div>`);
+    $(".card-img-container").append(`<img class="card-img">`);
+    $(".card-img").attr("src", "https://placehold.it/90x90");
+    $(".card-img").attr("alt", "profile picture");
+    $(".card").append(` <div class="card-info-container"></div>`);
+    $(".card-info-container").append(`<h3 id="name" class="card-name cap">${name.first}  ${name.last}</h3>`);
+    $(".card-info-container").append(`<p class="card-text">email</p>`);
+    $(".card-info-container").append(`<p class="card-text cap">${location.city}, ${location.state}</p>`);
+}
+// function generateProfile(results) {
+//     results.map(result => {
+generateHTML();
+
+//     });
+
+/* 
+
+function generateHTML(data) {
+const section = document.createElement('section');
+peopleList.appendChild(section);
+section.innerHTML = `
+<img src=${data.thumbnail.source}>
+<h2>${data.title}</h2>
+<p>${data.description}</p>
+<p>${data.extract}</p>
+`;
+}*/
 
 //Modal Window
 
@@ -47,3 +99,17 @@ xhr.send();
                     <p class="modal-text">Birthday: 10/21/2015</p>
                     </div>
                 </div> */}
+
+// var employees = JSON.parse(xhr.responseText);  //json.parse takes string and tries to convert it to var statusHTML = '<ul class="bulleted">';
+// var statusHTML = '<ul class="bulleted">';
+// for (let i = 0; i < employees.length; i += 1) {
+//     if (employees[i].inoffice === true) {
+//         statusHTML += '<li class="in">';
+//     } else { statusHTML += '<li lcass="out">'; }
+
+//     statusHTML += employees[i].name;
+// }
+// statusHTML += '</li>';  
+// }
+// statusHTML += '</ul>';
+// document.getElementById('employeeList').innerHTML = statusHTML;
