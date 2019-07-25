24a085c8a3aec784dcc1e1aeb7e50ff70842e920
jasminetaylor2 <51241456+jasminetaylor2@users.noreply.github.com>
2019-07-25 14:56:57 -0400

    works yo

--- js/apiScript.js
+++ js/apiScript.js
@@ -9,7 +9,7 @@ $(".header-text-container").prepend(`<h3>JASMINE TAYLOR'S</h3>`);
 // for your search feature, but you must use JS to create and
 // append it to`search-container` div.
 
-$(".search-container")
+$(".search-container")                                              //created the search input element in the html
     .html(`<form action="#" method="get">
         <input type="search" id="search-input" class="search-input" placeholder="Search...">
         <input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit">
@@ -88,49 +88,18 @@ getAllEmployees();
 
 
 
-// < !-- =======================
-//     Modal markup:
-
-// You can use the commented out markup below as a template
-// for your modal, but you must use JS to create and append
-// it to`body`.
-function createModal(cardData) {
-  //  let cardName = e.currentTarget.attr('index');
-   // let cardPicked = usersList[cardName];
-    //${cardData.picture.thumbnail}
-  //arrayResult.result.map( people => {  let modalcard = $('body')
-    //         .append(`<div class="modal-container">
-    //              <div class="modal">
-    //              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    //              <div class="modal-info-container">
-    //         <img class="modal-img" src="picture" alt="profile picture"> 
-    //             <h3 id="name" class="modal-name cap">name</h3>
-    //             <p class="modal-text">email</p>
-    //             <p class="modal-text cap">city</p>
-    //             <hr>
-    //                 <p class="modal-text">(555) 555-5555</p>
-    //                 <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-    //                 <p class="modal-text">Birthday: 10/21/2015</p>
-    //                 </div>
-    //             </div>`);
-    // $('body').append(modalcard)})
- //e.currenttarget.map()
-  //return modalcard;
-    
-
-
-}
-
 //create modal
 $(".gallery").on("click", ".card", function (e) {
     let i = e.currentTarget
     let picture = i.firstElementChild.firstElementChild.currentSrc
     let name = i.children[1].firstElementChild.innerHTML
     let email = i.firstElementChild.nextElementSibling.children[1].innerHTML
-    let city = i.children[1].children[5].innerHTML //i.children[1].children[2].innerHTML currentTarget.children[1].children[5]
-    let phone = i.children[1].children[3].innerHTML               //i.children[1].children[3].firstChild.innerHTML
+    let city = i.children[1].children[5].innerHTML 
+    let phone = i.children[1].children[3].innerHTML               
     let address = i.children[1].children[4].innerHTML
     let bDay = i.children[1].children[7].innerHTML
+    let handleOb; 
+    //holds the information that populates the modal window
     let modalcard = $('body')
         .append(`<div class="modal-container">
                  <div class="modal">
@@ -146,21 +115,40 @@ $(".gallery").on("click", ".card", function (e) {
                     <p class="modal-text">Birthday: ${bDay} </p>
                     </div>
                 </div>`);
-    document.getElementById("add").style.textTransform = "capitalize"
+    document.getElementById("add").style.textTransform = "capitalize"  //transform every first letter to capital 
     $('body').append(modalcard)
-    // createModal(i);
-        $(".modal-info-container").append(`<div class="modal-btn-container">
+  //creates the next/ prev buttons and append them to the modal
+        $("div.modal").append(`<div class="modal-btn-container">                   
         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
         <button type="button" id="modal-next" class="modal-next btn">Next</button>
     </div>`)
-    $(".phone").show();
-    $(".city").show()
-    $(".state").show();
-    $(".dob").show()
-    $(".postcode").show()
-    $(".address").show()
-    //     closeModal();
-    // var children = e.currentTarget.index();
+  
+        $(".phone").show();
+        $(".city").show()
+        $(".state").show();
+        $(".dob").show()
+        $(".postcode").show()
+        $(".address").show()
+ 
+        
+    //Modal strong button
+    //calls the closeModal function when button clicked
+    const xButton = document.querySelector("button");
+    xButton.className = "modal-close-btn";
+    xButton.onclick = () => {
+       closeModal()                                         
+    }
+    
+    //Next and Prev modal buttons
+    //shows the info for the employee card details before or after it
+    const prev = document.getElementById("modal-prev")
+    const next = document.getElementById("modal-next")
+    const card = document.getElementById("btn")
+    prev.className = "modal-prev btn";
+    prev.onclick = () => { console.log('u clicked prev button')}
+    
+    next.className = "modal-next btn";
+    next.onclick = () => {console.log('u clicked next button') }
 
     console.log(e);
 
@@ -169,23 +157,29 @@ $(".gallery").on("click", ".card", function (e) {
 
 //close modal
 function closeModal() {
-    //let xButton = document.getElementsByTagName('strong')
-    $(".modal-info-container").click(function () {
-        // $("div.modal").prop("disabled", false)
-        // if ($("div.modal").value = false) {
-        // $("div.modal").css({ "display": "none" });
-        $(".modal-info-container").hide();
-        console.log('heard');
-    })
-
-
+    $('.modal-container').remove();                 //removes the entire modal element when called
+    $(".phone").hide();                             //hides the extra employee info when modal closed
+    $(".address").hide();
+    $(".city").hide()
+    $(".state").hide();
+    $(".dob").hide()
+    $(".postcode").hide()
+    console.log('you closed the modalContainer')
 }
 
-// function modalButton(selector, idx) { 
+//  function modalButton(selector, idx) {
+//     let arrayIndex = 0
+
+// prev.onclick = function () {
 //     $(selector).attr('idx', 'idx');
 //     $(selector).prop("disabled", false)
-
+//     console.log(modalbuttons)
 // }
 
+// }
+ 
 
+$("body").css({ "background-color": "lightblue", })
+$("header").css({ "border-bottom-style": "solid", "border-color": "white" })
+//$(".header-text-container").css()
 
